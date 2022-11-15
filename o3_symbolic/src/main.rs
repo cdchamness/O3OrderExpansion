a95900460aaa6b73fa03ea956ef983b1ab631c99
cdchamness <113321462+cdchamness@users.noreply.github.com>
2022-11-14 19:46:49 -0500

    added lapplacian calculation to term, started work on adding terms

--- o3_symbolic/src/main.rs
+++ o3_symbolic/src/main.rs
@@ -1,23 +1,22 @@
 mod bra_ket;
 mod inner_product;
-mod term;
 mod kdelta;
+mod term;
 
 use crate::bra_ket::*;
 use crate::inner_product::*;
 use crate::term::*;
 
-
 fn main() {
-    let basic_bra = BraKet::new('l', 'y', vec![0,0]);
-    let basic_ket = BraKet::new('l', 'y', vec![0,1]);
-    
-    let basic_bra2 = BraKet::new('l', 'y', vec![0,1]);
-    let basic_ket2 = BraKet::new('l', 'y', vec![1,1]);
-    
+    let basic_bra = BraKet::new('l', 'y', vec![0, 0]);
+    let basic_ket = BraKet::new('l', 'y', vec![0, 1]);
+
+    let basic_bra2 = BraKet::new('l', 'y', vec![0, 1]);
+    let basic_ket2 = BraKet::new('l', 'y', vec![1, 1]);
+
     let basic_ip = InnerProduct::new(3.0, basic_bra, Vec::new(), basic_ket, None);
     let basic_ip2 = InnerProduct::new(3.0, basic_bra2, Vec::new(), basic_ket2, None);
-    let mut new_term = Term::new(vec![basic_ip, basic_ip2]);
+    let mut new_term = Term::new(vec![basic_ip.clone(), basic_ip2.clone()]);
     new_term.scalar_reduce();
     println!("Starting term: {}\n", new_term);
 
@@ -32,12 +31,17 @@ fn main() {
             ddt.collapse_all_deltas();
             let terms = ddt.alpha_reduce('a');
             for mut t in terms {
-            	t.scalar_reduce();
-            	println!("{}", t);
+                t.scalar_reduce();
+                println!("{}", t);
             }
         }
     }
-    
 
+    println!("First implementation of Lapp\n\n\n\n\nNow time for the second: \n");
 
+    let mut new_term = Term::new(vec![basic_ip, basic_ip2]);
+    let lapp = new_term.lapalacian('x', 'a');
+    for t in lapp {
+        println!("{}", t);
+    }
 }
--- o3_symbolic/src/term.rs
+++ o3_symbolic/src/term.rs
@@ -1,17 +1,15 @@
 use std::fmt;
-use std::ops::Mul;
+use std::ops::{Add, Mul};
 
-use crate::kdelta::*;
 use crate::inner_product::*;
-
+use crate::kdelta::*;
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct Term {
-    ips: Vec<InnerProduct>
+    ips: Vec<InnerProduct>,
 }
 
 impl Term {
-
     pub fn new(ips: Vec<InnerProduct>) -> Term {
         Term { ips }
     }
@@ -32,7 +30,7 @@ impl Term {
         let mut ips = Vec::new();
         for ip in &self.ips {
             ips.push(ip.clone())
-        } 
+        }
         Term::new(ips)
     }
 
@@ -45,10 +43,29 @@ impl Term {
             for d_ip in d_ips {
                 out.push(d_ip * self.duplicate());
             }
-            self.ips.insert(0,ip);
+            self.ips.insert(0, ip);
         }
-        
+
         out
+    }
+
+    pub fn lapalacian(&mut self, partial_index_type: char, alpha_type: char) -> Vec<Term> {
+        let mut lapp = Vec::new();
+        let d_terms = self.partial(partial_index_type, alpha_type);
+        for mut dt in d_terms {
+            dt.collapse_all_deltas();
+            let dd_terms = dt.partial(partial_index_type, alpha_type);
+            for mut ddt in dd_terms {
+                ddt.scalar_reduce();
+                ddt.collapse_all_deltas();
+                let out = ddt.alpha_reduce(alpha_type);
+                for mut t in out {
+                    t.scalar_reduce();
+                    lapp.push(t);
+                }
+            }
+        }
+        lapp
     }
 
     pub fn collapse_delta(&mut self, delta: &KDelta) {
@@ -56,7 +73,6 @@ impl Term {
             ip.collapse_delta(delta);
         }
     }
-
 
     pub fn collapse_all_deltas(&mut self) {
         while let Some(next_delta) = self.get_next_delta() {
@@ -68,7 +84,7 @@ impl Term {
         for ip in &self.ips {
             match ip.get_delta() {
                 Some(delta) => return Some(delta),
-                None => {},
+                None => {}
             }
         }
         None
@@ -76,7 +92,7 @@ impl Term {
 
     fn get_alpha_count(&self, alpha: char) -> Vec<usize> {
         let mut count: Vec<usize> = Vec::new();
-        for ip in &self.ips {    
+        for ip in &self.ips {
             let mut counts = 0;
             for gen in ip.get_inner() {
                 if gen.get_type() == alpha {
@@ -91,21 +107,19 @@ impl Term {
     pub fn alpha_reduce(&mut self, alpha: char) -> Vec<Term> {
         let inner_count = self.get_alpha_count(alpha);
         if inner_count.iter().sum::<usize>() == 2 {
-            
             // if there are 2 inners, either they are on the same InnerProduct or they are split
             if let Some(max_value) = inner_count.iter().max() {
                 if *max_value == 2 {
                     // They are both on the same InnerProduct
-                    let index = inner_count.iter().position( |&x| x == 2 ).unwrap();
+                    let index = inner_count.iter().position(|&x| x == 2).unwrap();
                     self.ips[index].clear_inner_alpha(alpha);
                     self.ips[index] *= -2.0;
 
                     // duplicate self, change ips at index to new_ip
                     vec![self.duplicate()]
-                }
-                else {
+                } else {
                     // They are split across multiple InnerProducts
-                    let mut alpha_indices = Vec::new(); 
+                    let mut alpha_indices = Vec::new();
                     let mut other_indices = Vec::new();
                     for (i, x) in inner_count.iter().enumerate() {
                         if *x == 1 {
@@ -121,7 +135,7 @@ impl Term {
                     }
                     let other_term = Term { ips: other_ips };
 
-                    // indices should have 2 
+                    // indices should have 2
                     let ip1 = &self.ips[alpha_indices[0]];
                     let ip2 = &self.ips[alpha_indices[1]];
 
@@ -132,19 +146,16 @@ impl Term {
                     out.push(other_term * term2);
                     out
                 }
-            }
-            else {
+            } else {
                 // Should not be possible to ever reach
                 unreachable!("max is only called if the sum == 2 => there must be a max");
             }
-        }
-        else { 
-            // if there are not 2 'inners' of the same type this does nothing. 
-            vec![self.duplicate()] 
+        } else {
+            // if there are not 2 'inners' of the same type this does nothing.
+            vec![self.duplicate()]
         }
     }
 }
-
 
 impl Mul<InnerProduct> for Term {
     type Output = Term;
@@ -152,6 +163,18 @@ impl Mul<InnerProduct> for Term {
     fn mul(self, rhs: InnerProduct) -> Term {
         let mut ips = self.ips;
         ips.push(rhs);
+        Term::new(ips)
+    }
+}
+
+impl Mul<f64> for Term {
+    type Output = Term;
+
+    fn mul(self, rhs: f64) -> Term {
+        let mut ips = self.ips;
+        let mut front = ips.remove(0);
+        front *= rhs;
+        ips.insert(0, front);
         Term::new(ips)
     }
 }
@@ -176,5 +199,20 @@ impl fmt::Display for Term {
         }
 
         write!(f, "{}", disp_string)
+    }
+}
+
+impl Add<Term> for Term {
+    type Output = Vec<Term>;
+
+    fn add(self, rhs: Term) -> Vec<Term> {
+        if self == rhs {
+            let mut cl = rhs.clone();
+            cl.scalar_reduce();
+            let scalar = cl.get_ips()[0].extract_scalar();
+            vec![self * scalar]
+        } else {
+            vec![self, rhs]
+        }
     }
 }
